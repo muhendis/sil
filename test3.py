4d7162f9f0759248d1646435178dd173be64ab03
sercan <enginbozaba1@gmail.com>
2021-04-30 02:12:16 +0300

    test kodları

---
+++ test3.py
@@ -0,0 +1,436 @@
+from __future__ import print_function, division
+# pip install future
+from future.utils import raise_from
+import time
+import argparse
+import cv2
+import torch
+import subprocess
+assert torch.__version__.split('.')[0] == '1'
+################################################# retinanet.dataloader #################################################
+import sys
+import os
+import torch
+import numpy as np
+import random
+import csv
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from torch.utils.data.sampler import Sampler
+
+# from pycocotools.coco import COCO
+
+import skimage.io
+import skimage.transform
+import skimage.color
+import skimage
+
+from PIL import Image
+
+
+class CSVDataset(Dataset):
+    """CSV dataset."""
+
+    def __init__(self, train_file, class_list, transform=None):
+        """
+        Args:
+            train_file (string): CSV file with training annotations
+            annotations (string): CSV file with class list
+            test_file (string, optional): CSV file with testing annotations
+        """
+        self.train_file = train_file
+        self.class_list = class_list
+        self.transform = transform
+
+        # parse the provided class file
+        try:
+            with self._open_for_csv(self.class_list) as file:
+                self.classes = self.load_classes(csv.reader(file, delimiter=','))
+        except ValueError as e:
+            raise_from(ValueError('invalid CSV class file: {}: {}'.format(self.class_list, e)), None)
+
+        self.labels = {}
+        for key, value in self.classes.items():
+            self.labels[value] = key
+
+        # csv with img_path, x1, y1, x2, y2, class_name
+        try:
+            with self._open_for_csv(self.train_file) as file:
+                self.image_data = self._read_annotations(csv.reader(file, delimiter=','), self.classes)
+        except ValueError as e:
+            print("self.train_file", self.train_file)
+            raise_from(ValueError('invalid CSV annotations file: {}: {}'.format(self.train_file, e)), None)
+        self.image_names = list(self.image_data.keys())
+
+    def _parse(self, value, function, fmt):
+        """
+        Parse a string into a value, and format a nice ValueError if it fails.
+        Returns `function(value)`.
+        Any `ValueError` raised is catched and a new `ValueError` is raised
+        with message `fmt.format(e)`, where `e` is the caught `ValueError`.
+        """
+        try:
+            return function(value)
+        except ValueError as e:
+            raise_from(ValueError(fmt.format(e)), None)
+
+    def _open_for_csv(self, path):
+        """
+        Open a file with flags suitable for csv.reader.
+        This is different for python2 it means with mode 'rb',
+        for python3 this means 'r' with "universal newlines".
+        """
+        if sys.version_info[0] < 3:
+            return open(path, 'rb')
+        else:
+            return open(path, 'r', newline='')
+
+    def load_classes(self, csv_reader):
+        result = {}
+
+        for line, row in enumerate(csv_reader):
+            line += 1
+
+            try:
+                class_name, class_id = row
+            except ValueError:
+                raise_from(ValueError('line {}: format should be \'class_name,class_id\''.format(line)), None)
+            class_id = self._parse(class_id, int, 'line {}: malformed class ID: {{}}'.format(line))
+
+            if class_name in result:
+                raise ValueError('line {}: duplicate class name: \'{}\''.format(line, class_name))
+            result[class_name] = class_id
+        return result
+
+    def __len__(self):
+        return len(self.image_names)
+
+    def __getitem__(self, idx):
+
+        img = self.load_image(idx)
+        annot = self.load_annotations(idx)
+        sample = {'img': img, 'annot': annot, "path": self.image_names[idx]}
+        if self.transform:
+            sample = self.transform(sample)
+        sample["path"] = self.image_names[idx]  # ben ekledim
+        return sample
+
+    def load_image(self, image_index):
+        # orjinal hali ---> https://github.com/yhenon/pytorch-retinanet/blob/master/retinanet/dataloader.py#L214
+        # engin
+
+        img = Image.open(self.image_names[image_index])
+
+        img = img.convert('RGB')
+
+        return np.array(img).astype(np.float32) / 255.0
+
+    def load_annotations(self, image_index):
+        # get ground truth annotations
+        annotation_list = self.image_data[self.image_names[image_index]]
+        annotations = np.zeros((0, 5))
+
+        # some images appear to miss annotations (like image with id 257034)
+        if len(annotation_list) == 0:
+            return annotations
+
+        # parse annotations
+        for idx, a in enumerate(annotation_list):
+            # some annotations have basically no width / height, skip them
+            x1 = a['x1']
+            x2 = a['x2']
+            y1 = a['y1']
+            y2 = a['y2']
+
+            if (x2 - x1) < 1 or (y2 - y1) < 1:
+                continue
+
+            annotation = np.zeros((1, 5))
+
+            annotation[0, 0] = x1
+            annotation[0, 1] = y1
+            annotation[0, 2] = x2
+            annotation[0, 3] = y2
+
+            annotation[0, 4] = self.name_to_label(a['class'])
+            annotations = np.append(annotations, annotation, axis=0)
+
+        return annotations
+
+    def _read_annotations(self, csv_reader, classes):
+        result = {}
+        for line, row in enumerate(csv_reader):
+            line += 1
+
+            try:
+                img_file, x1, y1, x2, y2, class_name = row[:6]
+            except ValueError:
+                raise_from(ValueError(
+                    'line {}: format should be \'img_file,x1,y1,x2,y2,class_name\' or \'img_file,,,,,\''.format(line)),
+                    None)
+
+            if img_file not in result:
+                result[img_file] = []
+
+            # If a row contains only an image path, it's an image without annotations.
+            if (x1, y1, x2, y2, class_name) == ('', '', '', '', ''):
+                continue
+
+            x1 = self._parse(x1, int, 'line {}: malformed x1: {{}}'.format(line))
+            y1 = self._parse(y1, int, 'line {}: malformed y1: {{}}'.format(line))
+            x2 = self._parse(x2, int, 'line {}: malformed x2: {{}}'.format(line))
+            y2 = self._parse(y2, int, 'line {}: malformed y2: {{}}'.format(line))
+
+            # Check that the bounding box is valid.
+            if x2 <= x1:
+                raise ValueError('line {}: x2 ({}) must be higher than x1 ({})'.format(line, x2, x1))
+            if y2 <= y1:
+                raise ValueError('line {}: y2 ({}) must be higher than y1 ({})'.format(line, y2, y1))
+
+            # check if the current class name is correctly present
+            if class_name not in classes:
+                raise ValueError('line {}: unknown class name: \'{}\' (classes: {})'.format(line, class_name, classes))
+
+            result[img_file].append({'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2, 'class': class_name})
+        return result
+
+    def name_to_label(self, name):
+        return self.classes[name]
+
+    def label_to_name(self, label):
+        return self.labels[label]
+
+    def num_classes(self):
+        return max(self.classes.values()) + 1
+
+    def image_aspect_ratio(self, image_index):
+        image = Image.open(self.image_names[image_index])
+        return float(image.width) / float(image.height)
+
+
+def collater(data):
+    imgs = [s['img'] for s in data]
+    annots = [s['annot'] for s in data]
+    scales = [s['scale'] for s in data]
+    path = [s['path'] for s in data]  # ben ekledim
+
+    widths = [int(s.shape[0]) for s in imgs]
+    heights = [int(s.shape[1]) for s in imgs]
+    batch_size = len(imgs)
+
+    max_width = np.array(widths).max()
+    max_height = np.array(heights).max()
+
+    padded_imgs = torch.zeros(batch_size, max_width, max_height, 3)
+
+    for i in range(batch_size):
+        img = imgs[i]
+        padded_imgs[i, :int(img.shape[0]), :int(img.shape[1]), :] = img
+
+    max_num_annots = max(annot.shape[0] for annot in annots)
+
+    if max_num_annots > 0:
+
+        annot_padded = torch.ones((len(annots), max_num_annots, 5)) * -1
+
+        if max_num_annots > 0:
+            for idx, annot in enumerate(annots):
+                # print(annot.shape)
+                if annot.shape[0] > 0:
+                    annot_padded[idx, :annot.shape[0], :] = annot
+    else:
+        annot_padded = torch.ones((len(annots), 1, 5)) * -1
+
+    padded_imgs = padded_imgs.permute(0, 3, 1, 2)
+
+    return {'img': padded_imgs, 'annot': annot_padded, 'scale': scales, "path": path}
+
+
+class Resizer(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __init__(self, SCALE=1.8):
+        self.scale = SCALE
+
+    def __call__(self, sample, min_side=608, max_side=1024):
+        image, annots = sample['img'], sample['annot']
+        rows, cols, cns = image.shape
+        #
+        # smallest_side = min(rows, cols)
+        #
+        # # rescale the image so the smallest side is min_side
+        # scale = min_side / smallest_side
+        #
+        # # check if the largest side is now greater than max_side, which can happen
+        # # when images have a large aspect ratio
+        # largest_side = max(rows, cols)
+        #
+        # if largest_side * scale > max_side:
+        #     scale = max_side / largest_side
+        #
+        # # resize the image with the computed scale
+        # image = skimage.transform.resize(image, (int(round(rows * scale)), int(round((cols * scale)))))
+        # rows, cols, cns = image.shape
+        #
+        # pad_w = 32 - rows % 32
+        # pad_h = 32 - cols % 32
+        #
+        # new_image = np.zeros((rows + pad_w, cols + pad_h, cns)).astype(np.float32)
+        # new_image[:rows, :cols, :] = image.astype(np.float32)
+        scale = self.scale
+        image = skimage.transform.resize(image, (int(round(rows * scale)), int(round((cols * scale)))))
+        rows, cols, cns = image.shape
+
+        pad_w = 32 - rows % 32
+        pad_h = 32 - cols % 32
+
+        new_image = np.zeros((rows + pad_w, cols + pad_h, cns)).astype(np.float32)
+        new_image[:rows, :cols, :] = image.astype(np.float32)
+        annots[:, :4] *= scale
+
+        return {'img': torch.from_numpy(new_image), 'annot': torch.from_numpy(annots), 'scale': scale}
+
+
+class Normalizer(object):
+
+    def __init__(self):
+        self.mean = np.array([[[0.485, 0.456, 0.406]]])
+        self.std = np.array([[[0.229, 0.224, 0.225]]])
+
+    def __call__(self, sample):
+        image, annots = sample['img'], sample['annot']
+        return {'img': ((image.astype(np.float32) - self.mean) / self.std), 'annot': annots}
+
+
+class UnNormalizer(object):
+    def __init__(self, mean=None, std=None):
+        if mean == None:
+            self.mean = [0.485, 0.456, 0.406]
+        else:
+            self.mean = mean
+        if std == None:
+            self.std = [0.229, 0.224, 0.225]
+        else:
+            self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+
+class AspectRatioBasedSampler(Sampler):
+
+    def __init__(self, data_source, batch_size, drop_last):
+        self.data_source = data_source
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.groups = self.group_images()
+
+    def __iter__(self):
+        random.shuffle(self.groups)
+        for group in self.groups:
+            yield group
+
+    def __len__(self):
+        if self.drop_last:
+            return len(self.data_source) // self.batch_size
+        else:
+            return (len(self.data_source) + self.batch_size - 1) // self.batch_size
+
+    def group_images(self):
+        # determine the order of the images
+        order = list(range(len(self.data_source)))
+        order.sort(key=lambda x: self.data_source.image_aspect_ratio(x))
+
+        # divide into groups, one group = one batch
+        return [[order[x % len(order)] for x in range(i, i + self.batch_size)] for i in
+                range(0, len(order), self.batch_size)]
+
+
+print('CUDA available: {}'.format(torch.cuda.is_available()))
+
+
+# %run test3.py --dataset csv  --csv_classes class_list.csv  --csv_val val_annots.csv
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
+
+    parser.add_argument('--dataset', help='Dataset type, must be one of csv or coco.')
+    parser.add_argument('--coco_path', help='Path to COCO directory')
+    parser.add_argument('--csv_classes', help='Path to file containing class list (see readme)')
+    parser.add_argument('--csv_val', help='Path to file containing validation annotations (optional, see readme)')
+
+    parser.add_argument('--model', help='Path to model (.pt) file.')
+
+    parser = parser.parse_args(args)
+
+    dataset_val = CSVDataset(train_file=parser.csv_val, class_list=parser.csv_classes, transform=transforms.Compose([Normalizer(), Resizer(SCALE=1.5)]))
+
+    sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
+    dataloader_val = DataLoader(dataset_val, num_workers=1, collate_fn=collater, batch_sampler=sampler_val)
+
+    retinanet = torch.load(r"/home/sercan/test_code/csv_retinanet_49.pt")
+
+    use_gpu = True
+
+    retinanet = retinanet.cuda()
+
+    retinanet = torch.nn.DataParallel(retinanet).cuda()
+
+    retinanet.eval()
+
+    unnormalize = UnNormalizer()
+
+    def draw_caption(image, box, caption):
+
+        b = np.array(box).astype(int)
+        cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
+        cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+    st = time.time()
+    for idx, data in  enumerate(dataloader_val):
+        with torch.no_grad():
+            #st = time.time()
+            scores, classification, transformed_anchors = retinanet(data['img'].cuda().float())
+            subprocess.run([r"nvidia-smi","--query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used","--format=csv"])
+            # print('Elapsed time: {}'.format(time.time()-st))
+
+            idxs = np.where(scores.cpu()>0.5)
+            img = np.array(255 * unnormalize(data['img'][0, :, :, :])).copy()
+
+
+            img[img<0] = 0
+            img[img>255] = 255
+
+            img = np.transpose(img, (1, 2, 0))
+
+            img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2RGB)
+
+            for j in range(idxs[0].shape[0]):
+                bbox = transformed_anchors[idxs[0][j], :]
+                x1 = int(bbox[0])
+                y1 = int(bbox[1])
+                x2 = int(bbox[2])
+                y2 = int(bbox[3])
+                label_name = dataset_val.labels[int(classification[idxs[0][j]])]
+                draw_caption(img, (x1, y1, x2, y2), label_name)
+
+                cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+                # print(label_name)
+
+            cv2.waitKey(0)
+    print('Elapsed time: {}'.format(time.time()-st))
+ 
+
+
+
+if __name__ == '__main__':
+ main()
